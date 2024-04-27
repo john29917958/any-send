9b52bfcf8f71763f6aeef794e2d5c86ce206be45
TZU-CHAO WANG <john29917958@gmail.com>
2024-04-27 23:29:24 +0800

    Addd key to every elements of levels panel

--- src/app/components/LocationPanel/LocationPanel.js
+++ src/app/components/LocationPanel/LocationPanel.js
@@ -5,11 +5,17 @@ import Level from "./Level";
 const LocationPanel = (props) => {
   const fullPath = props.path;
   const tokens = fullPath.split(/[\/\\]+/g);
-  const segments = tokens.filter((token) => token);
+  const levels = tokens.filter((token) => token);
   let elements = [];
-  for (const segment of segments) {
-    elements.push(<Text style={styles.element}>/</Text>);
-    elements.push(<Level name={segment}></Level>);
+  let index = 0;
+  for (const level of levels) {
+    elements.push(
+      <Text key={"slash-" + index} style={styles.element}>
+        /
+      </Text>
+    );
+    elements.push(<Level key={"level-" + index} name={level}></Level>);
+    index += 1;
   }
 
   return <View style={styles.container}>{elements}</View>;
